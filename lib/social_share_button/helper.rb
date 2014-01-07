b53ebb07569be81176820f3196f78ebefced5b99
ruting <liuruting@gmail.com>
2014-01-07 21:27:30 +0800

    change div to span

--- lib/social_share_button/helper.rb
+++ lib/social_share_button/helper.rb
@@ -5,19 +5,19 @@ module SocialShareButton
       extra_data = {}
       rel = opts[:rel]
       html = []
-      html << "<div class='social-share-button' data-title='#{h title}' data-img='#{opts[:image]}' data-url='#{opts[:url]}'>"
+      html << "<span class='social-share-button' data-title='#{title}' data-img='#{opts[:image]}' data-url='#{opts[:url]}'>"
       
       SocialShareButton.config.allow_sites.each do |name|
         extra_data = opts.select { |k, _| k.to_s.start_with?('data') } if name.eql?('tumblr')
 
         link_title = t "social_share_button.share_to", :name => t("social_share_button.#{name.downcase}")
-        html << link_to("","#", {:rel => ["nofollow", rel],
+        html << link_to("","_blank", {:rel => ["nofollow", rel],
                                   "data-site" => name,
                                   :class => "social-share-button-#{name}",
                                   :onclick => "return SocialShareButton.share(this);",
                                   :title => h(link_title)}.merge(extra_data))
       end
-      html << "</div>"
+      html << "</span>"
       raw html.join("\n")
     end
   end
